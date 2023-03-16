631f6310e9f667dfdfa27e8338e1d7466740647e
LonelyWind <hai0394@outlook.com>
2023-03-16 16:56:27 +0800

    Upgrade runtime to net 6.0; Use "System.Text.Json" instead of "Newtonsoft.Json" to reduce program size; Move the publish option from the project to the publish configuration; Add  publish configuration that depends on the net 6.0 runtime;

--- GvasConverter/Program.cs
+++ GvasConverter/Program.cs
@@ -1,55 +1,72 @@
 ﻿using System;
 using System.IO;
 using System.Text;
+using System.Text.Encodings.Web;
+using System.Text.Json;
+using System.Text.Json.Serialization;
 using GvasFormat;
 using GvasFormat.Serialization;
 using GvasFormat.Serialization.UETypes;
-using Newtonsoft.Json;
-
 
 namespace GvasConverter
 {
-  class Program
-  {
-    static void Main(string[] args)
+    class Program
     {
-      if (args.Length == 0)
-      {
-        Console.WriteLine("\n> GvasConverter.exe \"your.sva\"");
-        return;
-      }
+        static void Main(string[] args)
+        {
+            try
+            {
+                if (args.Length == 0)
+                {
+                    Console.WriteLine("\n> GvasConverter.exe \"your.sva\"");
+                    return;
+                }
 
-      var GVASPath = args[0];
-      // var GVASPath = $"C:\\Users\\ajanuw\\Documents\\My Games\\Octopath_Traveler\\76561197960267366\\SaveGames\\SaveData6.sav";
-      var jsonoutpath = GVASPath + ".json";
+                var GVASPath = args[0];
+                var jsonOutPath = GVASPath + ".json";
 
-      // 解析
-      Gvas save = UESerializer.Read(File.Open(GVASPath, FileMode.Open, FileAccess.Read, FileShare.Read));
+                // 解析
+                Gvas save = UESerializer.Read(File.Open(GVASPath, FileMode.Open, FileAccess.Read, FileShare.Read));
 
-      // object to json
-      // var json = JsonConvert.SerializeObject(save, new JsonSerializerSettings{Formatting = Formatting.Indented}); 
-      var json = JsonConvert.SerializeObject(save, Formatting.Indented, new MyJsonConvert());
+                var jsonNode = JsonSerializer.SerializeToNode<Gvas>(save, new JsonSerializerOptions
+                {
+                    IncludeFields = true,
+                    MaxDepth = 64,
+                    Converters = { new UEPropJsonConvert() }
+                });
+                File.WriteAllText(jsonOutPath, jsonNode.ToJsonString(new JsonSerializerOptions
+                {
+                    WriteIndented = true,
+                    Encoder = JavaScriptEncoder.UnsafeRelaxedJsonEscaping,
+                }));
+                Console.WriteLine("Convert success, json: " + jsonOutPath);
 
-      var writer = new StreamWriter(File.Open(jsonoutpath, FileMode.Create, FileAccess.Write, FileShare.Read), new UTF8Encoding(true));
-      writer.Write(json);
-      writer.Close();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Convert failed, exception: " + e);
+            }
+            finally
+            {
+                Console.Write("Press any key to quit. . .");
+                Console.Read();
+            }
+        }
     }
 
-  }
-
-  /// <summary>
-  /// 优化json输出
-  /// </summary>
-  public class MyJsonConvert : JsonConverter<UEProperty>
-  {
-    public override UEProperty ReadJson(JsonReader reader, Type objectType, UEProperty existingValue, bool hasExistingValue, JsonSerializer serializer)
+    /// <summary>
+    /// 优化json输出
+    /// </summary>
+    public class UEPropJsonConvert : JsonConverter<UEProperty>
     {
-      throw new NotImplementedException();
-    }
+        public override UEProperty? Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+        {
+            throw new NotImplementedException();
+        }
 
-    public override void WriteJson(JsonWriter writer, UEProperty value, JsonSerializer serializer)
-    {
-      writer.WriteRawValue(JsonConvert.SerializeObject(value.ToObject(), Formatting.Indented));
+        public override void Write(Utf8JsonWriter writer, UEProperty value, JsonSerializerOptions options)
+        {
+            writer.WriteRawValue(JsonSerializer.SerializeToUtf8Bytes(value.ToObject(), options));
+        }
     }
-  }
 }
